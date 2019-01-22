de48ac225bd42614df8ff2cae7d4bff3110fde2d
NEH <erik@bodigiti.com>
2019-01-22 09:45:48 -0500

    Snipcart-Custom.js: Reordered as 'On-page (not modal)' and 'Modal' and 'In-Active'/commented.

--- user/themes/bodigiti/js/source/snipcart-custom.js
+++ user/themes/bodigiti/js/source/snipcart-custom.js
@@ -1,14 +1,15 @@
 
-/**
-* Snipcart: Cart
-*/
-
 document.addEventListener("snipcart.ready", function() {
 
-  /**
-   * Snipcart Wrapper: If cart-not-empty add class for css add background color
-   * --------------------------------------------------------------------------
-   */
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  // On-Page (not modal)
+  // 
+  //
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Snipcart Wrapper: If cart-not-empty add class for css add background color
 
   var updateHeader = function(){
     if(Snipcart.api.items.count() !== 0){
@@ -26,13 +27,59 @@ document.addEventListener("snipcart.ready", function() {
   });
   updateHeader();
 
-  /**
-   * Size Options + Button Add-to-cart
-   * --------------------------------------------------------------------------
-   */
+  //
+  //
+  // END: On-Page (not modal)
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  // Modal
+  // 
+  //
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Popup Continue Shopping button: show it
+
+  Snipcart.execute('config', 'show_continue_shopping', true);
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Popup Close on mouse-click outside of the popup
+  //
+  // It verifies that the click isn't on the the container or the container's child.
+  // If it's not, it clicks on the #snipcart-close component to properly close the modal
+
+  $(document).mouseup(function(e) {
+    var container = $(".snip-layout__main-container");
+    if(!container.is(e.target) && container.has(e.target).length === 0) {
+      $("#snipcart-close").click();
+    }
+  });
+
+  //
+  //
+  // END: Modal
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  // In-Active
+  // 
+  //
+
+  /*
+  //////////////////////////////////////////////////////////////////////////////
+  // Size Options + Button Add-to-cart
+  //////////////////////////////////////////////////////////////////////////////
 
   // Size Chooser + Error Message Hide if none selected (message shown based
-  // on button click below) */
+  // on button click below)
   $('#size-chooser').on("change", function(){
     // If select <option> .size class exists then add class 'active' and hide error message
     $(this).find('option.size:selected').addClass('active')
@@ -47,47 +94,23 @@ document.addEventListener("snipcart.ready", function() {
   // which allows adding to cart
   // Button + Size: validate size option was selected or throw alert
   $("#add-to-cart").on("click", function(e){
-    /* if */
+    // if
     $("#size-chooser").length ?
-      /* if / else */
+      // if / else
       $(".sizes option.active").length ? $(this).addClass("snipcart-add-item") : (e.preventDefault(),
-        $(this).removeClass("snipcart-add-item"), /* 'else' continued */
-        $(".error-message").fadeIn("fast").addClass("active")) /* 'else' end */
-    /* else */
+        $(this).removeClass("snipcart-add-item"), // 'else' continued 
+        $(".error-message").fadeIn("fast").addClass("active")) // 'else' end 
+    // else
     : $(this).addClass("snipcart-add-item");
   });
 
-
-  /**
-   * Size: add selected to popup, by adding attribute "item-custom1-value" to <button>
-   * --------------------------------------------------------------------------
-   */
-
+  // Size: add selected to popup, by adding attribute "item-custom1-value" to <button>
   $('#size-chooser').on("change", function() {
     $('button').data('item-custom1-value', $(this).val());
   });
-
-  /**
-   * Popup Continue Shopping button: show it
-   * --------------------------------------------------------------------------
-   */
-
-  Snipcart.execute('config', 'show_continue_shopping', true);
-
-  /**
-   * Popup Close on mouse-click outside of the popup
-   * --------------------------------------------------------------------------
-   */
-
-   // It verifies that the click isn't on the the container or the container's child.
-   // If it's not, it clicks on the #snipcart-close component to properly close the modal
-
-  $(document).mouseup(function(e) {
-    var container = $(".snip-layout__main-container");
-    if(!container.is(e.target) && container.has(e.target).length === 0) {
-      $("#snipcart-close").click();
-    }
-  });
-
+  //
+  // END: Size Options + Button Add-to-cart
+  //////////////////////////////////////////////////////////////////////////////
+  */
 
 }, false); /* End: Snipcart event listener */
