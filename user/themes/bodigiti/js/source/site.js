4a0e11c115da7d2c196fdce0c4a5fdfc413212af
NEH <erik@bodigiti.com>
2019-01-23 11:03:02 -0500

    1) Added test nutrition images. 2) site.js - releted duplicated and not-commented 'Taxonomy Capitalize First Letter' jquery.

--- user/themes/bodigiti/js/source/site.js
+++ user/themes/bodigiti/js/source/site.js
@@ -110,120 +110,112 @@ jQuery(function($) {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   });
 
-	/////////////////////////////////////////////////////////////////////////////
-	// Capitalize first letter only in sentence (string) used in taxonomy categories
-	/////////////////////////////////////////////////////////////////////////////
 
-	$('.tags-item').text(function(_, txt) {
-		return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
-	});
+  /*
+  /////////////////////////////////////////////////////////////////////////////
+  // Mobile menu drawer
+  /////////////////////////////////////////////////////////////////////////////
+
+  $('.js-toggle-menu').on('click', function(e) {
+      e.preventDefault();
+      if(jQuery(this).hasClass('is-active')) {
+          jQuery('body').removeClass('overlay-is-active');
+          jQuery('.js-toggle-menu, .overlay, .drawer').removeClass('is-active');
+      } else {
+          jQuery('body').addClass('overlay-is-active');
+          jQuery('.js-toggle-menu, .overlay, .drawer').addClass('is-active');
+      }
+  });
+
+  $('.mobile-nav a > span').on('click', function(e) {
+      e.preventDefault();
+      if(jQuery(this).parents('li').hasClass('is-active')) {
+          jQuery(this).parents('ul').find('li').removeClass('is-active');
+      } else {
+          jQuery(this).parents('ul').find('li').removeClass('is-active');
+          jQuery(this).parents('li').addClass('is-active');
+      }
+  });
+
+  $('.mobile-nav a').on('click', function(e) {
+      if(e.toElement !== this) {
+          return;
+  }
+      if(jQuery(this).is('[href*="#"]') && jQuery(this).parents('li').hasClass('current-menu-parent')) {
+          jQuery('body').removeClass('overlay-is-active');
+          jQuery('.js-toggle-menu, .overlay, .drawer').removeClass('is-active');
+      }
+  });
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Mobile Menu auto shut
+  /////////////////////////////////////////////////////////////////////////////
+
+  $(window).resize(function() {
+      if(jQuery(window).width() > 960) {
+          jQuery('.overlay, .drawer, .js-toggle-menu').removeClass('is-active');
+          jQuery('body').removeClass('overlay-is-active');
+      }
+  }).resize();
 
 
-   /*
-   /////////////////////////////////////////////////////////////////////////////
-   // Mobile menu drawer
-   /////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////
+  // Capitalize first letter only in sentence (string) used in taxonomy categories
+  /////////////////////////////////////////////////////////////////////////////
 
-    $('.js-toggle-menu').on('click', function(e) {
-        e.preventDefault();
-        if(jQuery(this).hasClass('is-active')) {
-            jQuery('body').removeClass('overlay-is-active');
-            jQuery('.js-toggle-menu, .overlay, .drawer').removeClass('is-active');
-        } else {
-            jQuery('body').addClass('overlay-is-active');
-            jQuery('.js-toggle-menu, .overlay, .drawer').addClass('is-active');
-        }
-    });
-
-    $('.mobile-nav a > span').on('click', function(e) {
-        e.preventDefault();
-        if(jQuery(this).parents('li').hasClass('is-active')) {
-            jQuery(this).parents('ul').find('li').removeClass('is-active');
-        } else {
-            jQuery(this).parents('ul').find('li').removeClass('is-active');
-            jQuery(this).parents('li').addClass('is-active');
-        }
-    });
-
-    $('.mobile-nav a').on('click', function(e) {
-        if(e.toElement !== this) {
-            return;
-		}
-        if(jQuery(this).is('[href*="#"]') && jQuery(this).parents('li').hasClass('current-menu-parent')) {
-            jQuery('body').removeClass('overlay-is-active');
-            jQuery('.js-toggle-menu, .overlay, .drawer').removeClass('is-active');
-        }
-    });
-
-   /////////////////////////////////////////////////////////////////////////////
-   // Mobile Menu auto shut
-   /////////////////////////////////////////////////////////////////////////////
-
-    $(window).resize(function() {
-        if(jQuery(window).width() > 960) {
-            jQuery('.overlay, .drawer, .js-toggle-menu').removeClass('is-active');
-            jQuery('body').removeClass('overlay-is-active');
-        }
-    }).resize();
-
-  
-
-   /////////////////////////////////////////////////////////////////////////////
-   // Capitalize first letter only in sentence (string) used in taxonomy categories
-   /////////////////////////////////////////////////////////////////////////////
-
-    $('.tags-item').text(function(_, txt) {
-      return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
-    });
-    //
-    //
-    // END Mobile Menu
-    //////////////////////////////////////////////////////////////////////////// 
-   */
-
-   /////////////////////////////////////////////////////////////////////////////
-   // Lightslider
-   /////////////////////////////////////////////////////////////////////////////
-   /* Disabled Lightslider
-    $('#lightSlider').lightSlider({
-        gallery: true,
-        item: 1,
-        speed: 600,
-        loop: true,
-        slideMargin: 0,
-        thumbItem: 8, // Width of thumbnails calculated by this setting
-        galleryMargin: 0,
-        thumbMargin: 2,
-        enableTouch: true, // zoom ... set as false when using zoom
-        freeMove: false, // not sure what freeMove is but disabled since slider fine without
-        enableDrag: true // Desktop mouse support, set to false when zoom enabled
-    });
-    */
-
-   /////////////////////////////////////////////////////////////////////////////
-   // Isotope
-   /////////////////////////////////////////////////////////////////////////////
-
-   // external js: isotope.pkgd.js, imagesloaded.pkgd.js
-   /* Disable Isotope
-    // init Isotope (only IF .grid.isotope exists, otherwise get js errors in
-    // console even though nothing is broken visually )
-    if ( $(".grid-isotope").length ) {
-      var grid = document.querySelector('.grid-isotope');
-
-      var iso = new Isotope( grid, {
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        masonry: {
-          columnWidth: '.grid-sizer'
-        }
-      });
-
-      // images loaded
-      imagesLoaded( grid ).on( 'progress', function() {
-        // layout Isotope after each image loads
-        iso.layout();
-      });
-    }
+  $('.tags-item').text(function(_, txt) {
+    return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
+  });
+  //
+  //
+  // END Mobile Menu
+  //////////////////////////////////////////////////////////////////////////// 
   */
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Lightslider
+  /////////////////////////////////////////////////////////////////////////////
+  /* Disabled Lightslider
+  $('#lightSlider').lightSlider({
+      gallery: true,
+      item: 1,
+      speed: 600,
+      loop: true,
+      slideMargin: 0,
+      thumbItem: 8, // Width of thumbnails calculated by this setting
+      galleryMargin: 0,
+      thumbMargin: 2,
+      enableTouch: true, // zoom ... set as false when using zoom
+      freeMove: false, // not sure what freeMove is but disabled since slider fine without
+      enableDrag: true // Desktop mouse support, set to false when zoom enabled
+  });
+  */
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Isotope
+  /////////////////////////////////////////////////////////////////////////////
+
+  // external js: isotope.pkgd.js, imagesloaded.pkgd.js
+  /* Disable Isotope
+  // init Isotope (only IF .grid.isotope exists, otherwise get js errors in
+  // console even though nothing is broken visually )
+  if ( $(".grid-isotope").length ) {
+    var grid = document.querySelector('.grid-isotope');
+
+    var iso = new Isotope( grid, {
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      masonry: {
+        columnWidth: '.grid-sizer'
+      }
+    });
+
+    // images loaded
+    imagesLoaded( grid ).on( 'progress', function() {
+      // layout Isotope after each image loads
+      iso.layout();
+    });
+  }
+  */
+
 });
